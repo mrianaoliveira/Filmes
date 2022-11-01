d154e5681be38e8827ec0f59ae085e45d91c0763
Mariana Oliveira <marianaoliveirafernandes11@gmail.com>
2022-11-01 17:11:55 -0300

    componente de filme feito

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import MovieCard from '../components/MovieCard'
+
+const moviesURL = import.meta.env.VITE_API
+const apiKey= import.meta.env.VITE_API_KEY
 
 const Home = () => {
+
+  const [topMovies, setTopmovies] = useState([])
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setTopmovies(data.results);
+  }
+
+  useEffect(() => {
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+
+    getTopRatedMovies(topRatedUrl);
+  },[]);
+
   return (
-    <div>Home</div>
+    <div className="container">
+      <h2 className="title">Melhores filmes</h2>
+      <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregando...</p>}
+        {
+          topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)
+        }
+      </div>
+    </div>
   )
 }
 
