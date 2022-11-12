922409ed9d6e17ee94ac6ab8a039ac6ecf5eb8c0
Mariana Oliveira <marianaoliveirafernandes11@gmail.com>
2022-11-12 17:35:16 -0300

    funcao de pesquisar livro

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,37 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi';
 
 import './styles/NavBar.css';
+import { useState } from 'react';
 
 function NavBar() {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(!search)
+      return
+
+    navigate(`/search?q=${search}`);
+    setSearch("");
+  }
+
   return (
     <div>
         <nav id='navbar'>
             <h2>
                 <Link to="/"> <BiCameraMovie /> MoviesLib</Link>
             </h2>
-            <form>
-                <input type="text" placeholder='Busque um filme' />
+            <form onSubmit={handleSubmit}>
+                <input type="text" 
+                placeholder='Busque um filme' 
+                onChange={(e) => setSearch(e.target.value)}
+                value={search}
+                />
                 <button type='submit'>
                   <BiSearchAlt2 />
                 </button>
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { useEffect ,useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+import MovieCard from '../components/MovieCard';
+
+const searchURL = import.meta.VITE_SEARCH;
+const apiKey = import.meta.VITE_API;
+
+import './styles/MovieGrid.css'
 
 const Search = () => {
+  const [searchParams] = useSearchParams();
+
+  const [movies, setMovies] = useState([]);
+  const query = searchParams.get("q");
+
   return (
-    <div>Search</div>
+    <div className="container">
+      <h2 className="title">
+        Resultados para: <span className='query-text'>{query}</span>
+        </h2>
+      <div className="movies-container">
+        {movies.length === 0 && <p>Carregando...</p>}
+        {
+          movies.length > 0 && movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)
+        }
+      </div>
+    </div>
   )
 }
 
